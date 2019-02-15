ac85c3cda19a111c62d828b6ddcc73902da7cbb3
Huggo TRICOTTET <hutricot@e2r10p14.42.fr>
2019-02-15 17:42:38 +0100

    tasvus mon zoom salo

--- ft_hook.c
+++ ft_hook.c
@@ -6,7 +6,7 @@
 /*   By: hutricot <hutricot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 13:18:02 by hutricot          #+#    #+#             */
-/*   Updated: 2019/02/15 14:52:36 by hutricot         ###   ########.fr       */
+/*   Updated: 2019/02/15 17:42:06 by hutricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,25 @@ static int where(int key, int x, int y,t_ptr *ptr)
 	if (key == 4 || key ==  1)
 	{
 		mlx_clear_window(ptr->mlx, ptr->win);
-		ptr->z1 = ptr->z * 0.5;
-        ptr->o[X] += -0.85;//ptr->c_o + (d_x / WIDTH) / ptr->z1  ;
-	//	ptr->netter = 50;
+		printf ("%f\n", ptr->z);
 		ptr->h *= 0.5;
+        ptr->c_o = (ptr->c_o + ((d_x * ptr->z) / WIDTH) * ((ptr->h - ptr->h * 2) / (ptr->h * ptr->h * 2)));
+		//ptr->z = 2.7 * ptr->h; 
+		//ptr->z = 2.7 * ptr->h; 
+		//ptr->z = 2.7 * ptr->h; 
+		ptr->o[X] += ptr->c_o;
+	//	ptr->netter = 50;
 		ft_mandelbrot(ptr);
 	}
 	if (key == 5)
 	{
 		mlx_clear_window(ptr->mlx, ptr->win);
-		ptr->o[X] = (x + WIDTH / 2) * 0.0018518;
-		ptr->o[Y] = (y + HEIGHT / 2) * 0.0020833;
+		ptr->h *= 2;
+		ptr->z1 = ptr->z * ptr->h;
+		ptr->c_o = (ptr->c_o + ((d_x /ptr->z1 ) / WIDTH) * ((ptr->h - ptr->h * 0.5) / (ptr->h * ptr->h * 0.5)));
+		ft_mandelbrot(ptr);
+		ptr->o[X] += ptr->c_o;
+		ptr->z = ptr->z1;	
 		ptr->netter = 50;
 		ptr->h /= 0.8;
 		ft_mandelbrot(ptr);
