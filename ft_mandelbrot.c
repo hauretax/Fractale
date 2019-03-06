938efa07f64b4d199a57edf0c33dae68553a9ea8
Huggo TRICOTTET <hutricot@e3r8p20.42.fr>
2019-03-06 18:29:01 +0100

    version fonctionelle du trhead

--- ft_mandelbrot.c
+++ ft_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: hutricot <hutricot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 11:56:14 by hutricot          #+#    #+#             */
-/*   Updated: 2019/03/06 17:53:53 by hutricot         ###   ########.fr       */
+/*   Updated: 2019/03/06 18:28:20 by hutricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,59 +44,107 @@ void *mandelbrot1(void *arg)
 	t_struct *s;
 
 	s = (t_struct *)arg;
+	s->v.i[Y] = 0;
+	while (s->v.i[Y] < HEIGHT)
+	{
 		s->v.i[X] = 0;
 		while (s->v.i[X] < WIDTH)
 		{
 			ft_mandelbrot(s);
-			s->v.i[X]++;
+			s->v.i[X] += 4;
 		}
+		s->v.i[Y]++;
+	}
+	pthread_exit (0);
+}
+
+void *mandelbrot2(void *arg)
+{
+	t_struct *s;
+
+	s = (t_struct *)arg;
+	s->v.i[Y] = 0;
+	while (s->v.i[Y] < HEIGHT)
+	{
+		s->v.i[X] = 1;
+		while (s->v.i[X] < WIDTH)
+		{
+			ft_mandelbrot(s);
+			s->v.i[X] += 4;
+		}
+		s->v.i[Y]++;
+	}
+	pthread_exit (0);
+}
+
+void *mandelbrot3(void *arg)
+{
+	t_struct *s;
+
+	s = (t_struct *)arg;
+	s->v.i[Y] = 0;
+	while (s->v.i[Y] < HEIGHT)
+	{
+		s->v.i[X] = 2;
+		while (s->v.i[X] < WIDTH)
+		{
+			ft_mandelbrot(s);
+			s->v.i[X] += 4;
+		}
+		s->v.i[Y]++;
+	}
+	pthread_exit (0);
+}
+
+void *mandelbrot4(void *arg)
+{
+	t_struct *s;
+
+	s = (t_struct *)arg;
+	s->v.i[Y] = 0;
+	while (s->v.i[Y] < HEIGHT)
+	{
+		s->v.i[X] = 3;
+		while (s->v.i[X] < WIDTH)
+		{
+			ft_mandelbrot(s);
+			s->v.i[X] += 4;
+		}
+		s->v.i[Y]++;
+	}
 	pthread_exit (0);
 }
 
 void	mandelbrot(t_struct *s)
 {
-	pthread_t th[HEIGHT];
-	t_struct *s_m[HEIGHT];
-	int i;
-	
-	i = 0;
-	while (i < 4)
-	{
-		s_m[i] = s;
-		i++;
-	}
+	pthread_t th[4];
+	t_struct s_m[4];
+
 	s->v.x1 = (s->v.o[0]);
 	s->v.x2 = (s->v.o[1]);
 	s->v.y1 = (s->v.o[2]);
 	s->v.y2 = (s->v.o[3]);
-	s->v.mx_i = 50 + s->v.acuracy;
+	s->v.mx_i = 1000 + s->v.acuracy;
 	if (s->v.acuracy <= 0)
 		s->v.acuracy = 0;
+	s_m[0] = *s;
+	s_m[1] = *s;
+	s_m[2] = *s;
+	s_m[3] = *s;
 
-	i = 0;
-	s->v.i[Y] = 0;
-	while (s->v.i[Y] < HEIGHT)
-	{
-		if (pthread_create (&th[i], NULL, mandelbrot1, s_m[i]) < 0)
-			exit(0);
-		s->v.i[Y]++;
-		i++;
-		if (i == 4)
-			{
-				i = 0;
-				while (i < 4)
-				{
-					pthread_join (th[i], NULL);
-					i++;
-				}
-				i = 0;
-			}
-	}
-	i = 0;
 		
-	while (i < 4)
-	{
-		pthread_join (th[i], NULL);
-		i++;
-	}
+		if (pthread_create (&th[0], NULL, mandelbrot1, &s_m[0]) < 0)
+			exit(0);
+		if (pthread_create (&th[1], NULL, mandelbrot2, &s_m[1]) < 0)
+			exit(0);
+		if (pthread_create (&th[2], NULL, mandelbrot3, &s_m[2]) < 0)
+			exit(0);
+		if (pthread_create (&th[3], NULL, mandelbrot4, &s_m[3]) < 0)
+			exit(0);
+		
+		
+		pthread_join (th[0], NULL);
+		pthread_join (th[1], NULL);
+		pthread_join (th[2], NULL);
+		pthread_join (th[3], NULL);
 }
